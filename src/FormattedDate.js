d7c0df46a565d3605f0e3ec9a77beb9bc653baed
Kate2021dar <smile555kd@gmail.com>
2022-10-23 19:14:32 +0300

    update FormattedDate and FormattedMonth

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function FormattedDate(props) {
   let days = ["Sunday", "Monday", "Tuesday", "Thursday", "Friday", "Saturday"];
   let day = days[props.date.getDay()];
